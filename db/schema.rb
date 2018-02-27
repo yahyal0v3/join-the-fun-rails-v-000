6133732fcbccaa1a57685df6788049221a65d90e
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-02-27 21:23:28 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140212205242) do
+ActiveRecord::Schema.define(version: 20140212205318) do
+
+  create_table "passengers", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "rides", force: :cascade do |t|
     t.datetime "created_at", null: false
